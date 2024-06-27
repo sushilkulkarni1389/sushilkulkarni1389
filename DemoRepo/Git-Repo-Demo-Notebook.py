c17ca92589a4e520599d06c55567072a352e6a6b
sushilkulkarni1389 <sushilkulkarni1389>
2024-06-27 05:04:03 +0000

    adding 1st notebook

---
+++ DemoRepo/Git-Repo-Demo-Notebook.py
@@ -0,0 +1,8 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC # Git Repo Demo Notebook
+# MAGIC # This notebook is created to demonstrate how git integration works with databricks
+
+# COMMAND ----------
+
+print("Hello World")
